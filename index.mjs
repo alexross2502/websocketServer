eb786742d91f5fcedc1c6da2278954735a1a05ff
alexross2502 <93192857+alexross2502@users.noreply.github.com>
2024-11-02 02:46:41 +0300

    test2

--- index.mjs
+++ index.mjs
@@ -7,26 +7,26 @@ const server = http.createServer(app);
 import { WebSocketServer } from "ws";
 import WebSocket from "ws";
 import { connectDB } from "./db.js";
-// import router from "./routes/index.js";
-// import cors from "cors";
-// import cookieParser from "cookie-parser";
-// import path from "path";
-// import { fileURLToPath } from "url";
-// import { dirname } from "path";
-// import jwt from "jsonwebtoken";
+import router from "./routes/index.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import jwt from "jsonwebtoken";
 import Users from "./models/Users.js";
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 connectDB();
 
-// app.use(
-//   cors({
-//     origin: "http://localhost:3000",
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 // app.use(cookieParser());
 // app.use(express.json());
