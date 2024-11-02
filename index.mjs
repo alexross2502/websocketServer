bcffc5c417d67209152436c667070f5195923f0f
alexross2502 <93192857+alexross2502@users.noreply.github.com>
2024-11-02 03:55:58 +0300

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -7,14 +7,14 @@ const app = express();
 //import { WebSocketServer } from "ws";
 //import WebSocket from "ws";
 import { connectDB } from "./db.js";
-//import router from "./routes/index.js";
+import router from "./routes/index.js";
 //import cors from "cors";
 //import cookieParser from "cookie-parser";
 //import path from "path";
 //import { fileURLToPath } from "url";
 //import { dirname } from "path";
 //import jwt from "jsonwebtoken";
-//import Users from "./models/Users.js";
+import Users from "./models/Users.js";
 
 //const __filename = fileURLToPath(import.meta.url);
 //const __dirname = dirname(__filename);
