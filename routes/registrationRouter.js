224bb951a92b00ee5c8a41417fa3c22920c9a992
alexross2502 <93192857+alexross2502@users.noreply.github.com>
2024-11-02 03:26:07 +0300

    dsadsadas

--- routes/registrationRouter.js
+++
@@ -1,7 +0,0 @@
-import * as express from "express";
-import * as registrationController from "../controllers/registrationController.js";
-const router = express.Router();
-
-router.post("/", registrationController.registration);
-
-export default router;
